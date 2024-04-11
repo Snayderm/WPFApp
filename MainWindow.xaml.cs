e0e136699e0f2ba9d52b99480e31642c96b332d9
Snayderm <sneyder1002m@gmail.com>
2024-04-11 02:31:35 -0500

    Test

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,1 +1,35 @@
-﻿
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+namespace WPFApp
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		void CreateTask()
+		{
+			Task T;
+			int i = 0;
+
+		}
+
+		void ShowMessage()
+		{
+			MessageBox.Show("Ejecutando el método ShowMessage");
+		}
+	}
+}
